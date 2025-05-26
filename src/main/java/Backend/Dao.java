de3ddb2adf5a448dcefb01f24d23f7cf4b962c7f
TomasEze <tomasezequielpenalozaluviano@gmail.com>
2025-05-25 20:54:51 -0600

    Update Dao.java

--- src/main/java/Backend/Dao.java
+++ src/main/java/Backend/Dao.java
@@ -1,10 +1,8 @@
 package Backend;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface Dao<T>{
-    Optional<T> get(long id);
     List<T> obtenerTodos();
     void guardar(T t);
     void actualizar(T t, String[] params);
